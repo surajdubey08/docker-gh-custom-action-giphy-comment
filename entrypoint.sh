a638f920d781bca3d68b7663ad6831f422f306d4
Suraj Dhar Dubey <surajdhar08.dubey@gmail.com>
2025-04-14 14:05:11 +0530

    Fixed entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ GITHUB_TOKEN=$1
 GIPHY_API_KEY=$2
 
 # Get the pull request number from the GitHub Event Payload
-pull_request_number=$(jq --raw-output .pull_rqeuest.number "$GITHUB_EVENT_PATH")
+pull_request_number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
 echo PR Number = $pull_request_number
 
 # Use the Giphy API to fetch a random Thank You GIF
